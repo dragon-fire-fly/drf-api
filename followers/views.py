5c959443038fe48c9c4354c196dabb15349a9cb5
dragon-fire-fly <ek93@hotmail.co.uk>
2023-04-12 15:21:14 +0200

    Add follower app model, serializer, views & urls

---
+++ followers/views.py
@@ -0,0 +1,19 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from .serializers import FollowerSerializer
+from .models import Follower
+from drf_api.permissions import IsOwnerOrReadOnly
+
+
+class FollowerList(ListCreateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
