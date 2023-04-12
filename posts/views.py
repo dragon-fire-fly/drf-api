450ad103a1af4c819242b4754d8cf58d35891427
dragon-fire-fly <ek93@hotmail.co.uk>
2023-04-12 17:12:18 +0200

    Add filtering and searching functionality

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -9,9 +10,26 @@ class PostList(generics.ListCreateAPIView):
     List posts or create a post if logged in
     The perform_create method associates the post with the logged in user.
     """
+
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count("owner__comment", distinct=True),
+        likes_count=Count("owner__like", distinct=True),
+    ).order_by("-created_at")
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        "owner__username",
+        "title"
+    ]
+    ordering_fields = [
+        "comments_count",
+        "likes_count",
+        "likes__created_at",
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -21,6 +39,10 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a post and edit or delete it if you own it.
     """
+
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count("owner__comment", distinct=True),
+        likes_count=Count("owner__like", distinct=True),
+    ).order_by("-created_at")
