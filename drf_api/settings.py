1c4302f7e83334a837f519a73a8d296a32e92651
dragon-fire-fly <ek93@hotmail.co.uk>
2023-04-19 10:09:21 +0200

    Add global date time formatting and for comments

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -23,12 +23,22 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [(
-        "rest_framework.authentication.SessionAuthentication" 
-        if "DEV" in os.environ 
-        else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
-    )]
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        (
+            "rest_framework.authentication.SessionAuthentication"
+            if "DEV" in os.environ
+            else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
+        )
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%d %b %Y",
 }
+if "DEV" not in os.environ:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "rest_framework.renderers.JSONRenderer",
+    ]
+
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -36,7 +46,7 @@ JWT_AUTH_COOKIE = "my-app-auth"
 JWT_AUTH_REFRESH_COOKIE = "my refresh-token"
 
 REST_AUTH_SERIALIZERS = {
-    "USER_SETAILS_SERIALIZER": "drf_api.serializers.CurrentUserSerializer"
+    "USER_DETAILS_SERIALIZER": "drf_api.serializers.CurrentUserSerializer"
 }
 
 # Quick-start development settings - unsuitable for production
@@ -66,7 +76,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework.authtoken",
     "dj_rest_auth",
-    'django.contrib.sites',
+    "django.contrib.sites",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
